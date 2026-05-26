c46fb65e991634e0ab89ad38559506280cddd27b
ChristianSalazar07 <christian.morillo@aluno.senac.rio>
2026-05-25 21:46:05 -0300

    Adicionados Models de Aluno

--- aluno/models.py
+++ aluno/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Aluno(models.Model):
+    nome = models.CharField(max_length=100)
+    data_matricula = models.DateField()
+
+    def __str__(self):
+        return self.nome
